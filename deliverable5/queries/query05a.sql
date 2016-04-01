98f05d66dbd5ab23ebecd5c21af1f9862be5b9d6
Jonathan Glines <auntieNeo@gmail.com>
2016-04-01 13:19:39 -0600

    Added query05a, count of equipment rentals by type. Haven't tested case with no rentals.

---
+++ deliverable5/queries/query05a.sql
@@ -0,0 +1,7 @@
+USE twk;
+SELECT et.equipment_type_code, equipment_type_description, COUNT(contract_num)
+  FROM equipment_type AS et LEFT JOIN equipment AS e
+    ON et.equipment_type_code = e.equipment_type_code
+  LEFT JOIN rental_item AS ri
+    ON e.equipment_id = ri.equipment_id
+  GROUP BY equipment_type_code;
