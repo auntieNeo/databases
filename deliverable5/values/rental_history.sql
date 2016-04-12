d88b629bd722351c4b4c1ad6d516b9317e2a1466
Jonathan Glines <auntieNeo@gmail.com>
2016-04-12 16:50:32 -0600

    Added rental incidents to make more convincing

--- deliverable5/values/rental_history.sql
+++ deliverable5/values/rental_history.sql
@@ -1,7 +1,15 @@
 use twk;
 
-insert into rental_history (customer_num, rental_date, problem_description, amount_owed) values (1,'2016-03-18','No problem.',70.00);
-insert into rental_history (customer_num, rental_date, problem_description, amount_owed) values (2,'2016-01-15','Good condition.',35.00);
-insert into rental_history (customer_num, rental_date, problem_description, amount_owed) values (3,'2016-02-29','Scuffed, but usable.',60.00);
-insert into rental_history (customer_num, rental_date, problem_description, amount_owed) values (4,'2016-03-10','Like new.',40.00);
-insert into rental_history (customer_num, rental_date, problem_description, amount_owed) values (5,'2016-02-22','Like new.',70.00);
+insert into rental_history (customer_num, rental_date, problem_description, amount_owed) values (1,'2016-03-18','Broken paddle.',70.00);
+insert into rental_history (customer_num, rental_date, problem_description, amount_owed) values (2,'2015-01-15','Lifejacket Poor condition.',35.00);
+insert into rental_history (customer_num, rental_date, problem_description, amount_owed) values (2,'2015-01-16','Lifejacket Poor condition.',35.00);
+insert into rental_history (customer_num, rental_date, problem_description, amount_owed) values (2,'2015-01-26','Lifejacket Poor condition.',35.00);
+insert into rental_history (customer_num, rental_date, problem_description, amount_owed) values (2,'2015-01-25','Lost paddles.',35.00);
+insert into rental_history (customer_num, rental_date, problem_description, amount_owed) values (3,'2015-02-20','Scuffed canoe.',25.00);
+insert into rental_history (customer_num, rental_date, problem_description, amount_owed) values (3,'2015-03-15','Broken paddle.',60.00);
+insert into rental_history (customer_num, rental_date, problem_description, amount_owed) values (3,'2015-03-20','Torn life jacket.',30.00);
+insert into rental_history (customer_num, rental_date, problem_description, amount_owed) values (3,'2015-03-22','Missing life jacket.',30.00);
+insert into rental_history (customer_num, rental_date, problem_description, amount_owed) values (3,'2015-09-12','Lost canoe.',2500.00);
+insert into rental_history (customer_num, rental_date, problem_description, amount_owed) values (3,'2015-10-22','Broken paddle.',50.00);
+insert into rental_history (customer_num, rental_date, problem_description, amount_owed) values (4,'2016-03-10','Cracked hull.',40.00);
+insert into rental_history (customer_num, rental_date, problem_description, amount_owed) values (5,'2016-02-22','Cracked hull.',70.00);
